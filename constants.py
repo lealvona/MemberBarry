8b26189aeddf59b800e8225d4ba23b76d830487a
Leal Vona <lealvona.work@gmail.com>
2023-11-24 19:49:42 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,21 +1,24 @@
 DEFAULT_SYSTEM_PROMPT = 'You are a helpful assistant, named Barry.'
 
-GENERATE_SUMMARY_SYSTEM_PROMPT = """You are a summarizing assistant. You take a 
-long conversation and summarize it. Pay special attention to the details. Names, 
-places, times and recurring objects or themes are important. Pay attention to 
-verbs and actions. Summaries you create will be used to retain context in 
-ongoing conversations with an AI chat bot, so format the response in a way that 
-is useful for that purpose.
+GENERATE_SUMMARY_SYSTEM_PROMPT = """You are a summarizing assistant. You take
+ a long conversation and summarize it. Pay special attention to the details.
+ Names, places, times and recurring objects or themes are important. Pay
+ attention to verbs and actions. Lists are important. Summaries you create
+ will be used to retain context in ongoing conversations with an AI chat bot,
+ so format the response in a way that is useful for that purpose. Do not use
+ the word "summary" in your response.
 """
 
-GENERATE_SUMMARY_USER_PROMPT = """Please summarize everything we've discussed so far. 
-Just give me the summary with no preamble. Sterile tone. Include any and all 
-specific details. Do not include any references to me giving you summaries, 
-but use the content from any summaries I've provided. Make sure that every 
-question I have asked is reflected in the summary."""
+GENERATE_SUMMARY_USER_PROMPT = """Please summarize everything we've discussed
+ so far. Just give me the summary with no preamble. Sterile tone. Include any
+ and all specific details. Lists are important. Do not include any references
+ to me giving you summaries, but use the content from any summaries I've
+ provided. Make sure that every question I have asked is reflected in the
+ summary. Do not use the word "summary" in your response."""
 
-USE_SUMMARY_USER_PROMPT = """This is a summary of what we've discussed so far, 
-if you understand simply say OK.
+USE_SUMMARY_USER_PROMPT = """This is a summary of what we've discussed so
+ far, if you understand simply say OK.
 
-Summary: 
-"""  # There is a purposeful space at the end of this text.
+Summary:
+
+"""
